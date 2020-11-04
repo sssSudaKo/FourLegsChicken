875bf30da231faa916d398022da188cc89654f85
Yuetatsu <m0119140ba@edu.teu.ac.jp>
2020-11-04 10:12:42 +0900

    TPSカメラ&移動実装
    -TPS用カメラ&移動の実装
    -Scene1内オブジェクトの微調整
    -Scriptの整理

---
+++ Assets/Sasaki/foodMaker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodMaker : MonoBehaviour
+{
+    public GameObject food;
+    public int amountOfFood;
+
+    float x, y, z;
+    void Start()
+    {
+        for(int i = 0; i < amountOfFood; i++)
+        {
+            x = Random.Range(-140f, 140f);
+            y = Random.Range(2f, 10f);
+            z = Random.Range(-140f, 140f);
+
+            Instantiate(food, new Vector3(x, y, z), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Sasaki/goatSim_Move.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goatSim_Move : MonoBehaviour
+{
+    private CharacterController charaCon;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float gravity = 9.81f;
+
+    public float speed;
+    public float jumpHeight;
+    public float rotSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charaCon = gameObject.GetComponent<CharacterController>();　//変数charaConにCharacterController コンポネントを入れます
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        groundedPlayer = charaCon.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 camForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        Vector3 dir = camForward * Input.GetAxis("Vertical") + Camera.main.transform.right * Input.GetAxis("Horizontal");
+
+        charaCon.Move(dir * Time.deltaTime * speed);
+
+        if (dir != Vector3.zero)
+        {
+            Quaternion qua = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, qua, rotSpeed * Time.deltaTime);
+        }
+
+        // プレイヤーのジャンプの計算
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += jumpHeight * gravity;
+        }
+
+        playerVelocity.y -= gravity * Time.deltaTime;
+        charaCon.Move(playerVelocity * Time.deltaTime);
+
+    }
+}
